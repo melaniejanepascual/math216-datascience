59875544c8248d591c7173be2ab44891e1a7ad4f
MJ <melaniejane.pascual@gmail.com>
2017-09-18 08:48:30 -0400

    sep 18 lessons

---
+++ 9.18.R
@@ -0,0 +1,18 @@
+## Load in library of ggplot2 functions
+library(ggplot2)
+
+# Make scatterplot in ggplot2
+ggplot(data=diamonds, mapping=aes(x=carat, y=price)) + geom_point()
+
+# Make boxplot in ggplot2
+ggplot(data=diamonds, mapping=aes(x=factor(0), y=price)) + geom_boxplot()
+
+# conclusion: worst color diamonds are more expensive than better colored diamonds
+ggplot(data=diamonds, mapping=aes(x=color, y=price)) + geom_boxplot()
+
+# Make scatterplot with >2 variables
+# points turn red
+ggplot(data=diamonds, mapping=aes(x=carat, y=price)) + geom_point(color="red")
+
+# points based on color
+ggplot(data=diamonds, mapping=aes(x=carat, y=price)) + geom_point(aes(color=color))
