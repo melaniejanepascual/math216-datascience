bf9befa783755f4f80ccd4719069ea04a1618a60
MJ <melaniejane.pascual@gmail.com>
2017-09-22 08:19:14 -0400

    almost

---
+++ lectures/9.22.#.R
@@ -0,0 +1,22 @@
+library(tidyverse)
+
+## Make subsets of data
+subset(diamonds, color == "E" & carat > 1)
+
+filter(diamonds, color == "E", carat > 1)
+
+filter(diamonds, between(price, 5000, 10000))
+
+## Selects particular columns
+select(diamonds, carat, price)
+
+starts_with()
+select(diamonds, starts_with("c"))
+select(diamonds, ends_with("c"))
+
+
+## Lets say we want only the diamonds >$10000 in price.
+## Then, we want to examine their colors only.
+
+expensive.diamonds <- filter(diamonds, price > 10000)
+select(expensive.diamonds, color, price)
