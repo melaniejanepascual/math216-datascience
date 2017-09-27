8884456d5c9ea3f776aa67a60cda99713d6a56f0
MJ <melaniejane.pascual@gmail.com>
2017-09-27 09:02:42 -0400

    yes

--- lectures/9.27.R
+++ lectures/9.27.R
@@ -14,7 +14,25 @@ mean(flights$dep_delay > 60, na.rm = TRUE)
 # of flights delayed more than an hour. (dep delay)
 
 flights %>%
-  select(dep_delay) %>%
-  filter(dep_delay > 60) %>%
   group_by(carrier) %>%
-  summarize(mean(flights$carrier), na,rm=TRUE)
+  summarize(carrier.avg = mean(dep_delay > 60 , na.rm=TRUE)) %>%
+  arrange(desc(carrier.avg)) %>%
+  mutate(Rank = rank(-carrier.avg)) %>%
+  filter(Rank <= 10) %>%
+  ggplot(mapping = aes(x = reorder(carrier, carrier.avg), carrier.avg)) + 
+  geom_bar(stat = "identity", aes(fill = carrier))
+
+## Create a function
+favNum <- function(x) {
+  num <- paste(c("MJ's favorite number is", x, "."), collapse = " ")
+  return(num)
+}
+
+## Create a t test function - assume we only care about left tailed t tests
+pt <- function(data) {
+  x <- mean(data)
+  s <- sd(data)
+  
+  
+  pvalue <- (x-m)/(s/sqrt(n))
+}
