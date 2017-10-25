f4ffb97a41336519975865b709ee91c3b1e3c164
MJ <melaniejane.pascual@gmail.com>
2017-10-25 08:23:40 -0400

    shiny

---
+++ lectures/10.25/10.25/app.R
@@ -0,0 +1,24 @@
+#base code for every shiny app
+library(shiny)
+# controls everything the user will see - user interface!
+ui <- fluidPage(
+  sliderInput(inputId = "slider1", label = "Select a value",
+              min = 0, max = 10000, value = 5000),
+  plotOutput(outputId = "Histogram")
+  
+)
+# do math in server function 
+server <- function(input, output) {
+  # generate random normal variables with the number equal to slider value, then
+  # make a graph
+  
+  #everything you want to output needs to be stored in the output vector
+  output$Histogram <-renderPlot({
+    # generate random normal variables
+    random.variables <-rnorm(input$slider1)
+    hist(random.variables)
+  })
+}
+
+##everytime slider is changed, input is changed so a new histogram is made
+shinyApp(ui = ui, server = server) 
