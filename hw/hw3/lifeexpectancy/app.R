3949c99fbd263b1856fcc7813d573dcc91f2a53d
MJ <melaniejane.pascual@gmail.com>
2017-10-29 14:50:07 -0400

    hw 3

---
+++ hw/hw3/lifeexpectancy/app.R
@@ -0,0 +1,12 @@
+# Demonstrate the changes in life expectancy for each country/continent over time 
+# using a Shiny App.
+library(shiny)
+
+ui <- fluidPage(
+)
+
+server <- function(input, output) {
+}
+
+shinyApp(ui = ui, server = server)
+
