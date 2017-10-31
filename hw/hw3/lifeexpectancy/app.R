64ddb57ad71c743f276997e6c4e77c77f1b3554c
MJ <melaniejane.pascual@gmail.com>
2017-10-30 21:39:25 -0400

    more

--- hw/hw3/lifeexpectancy/app.R
+++ hw/hw3/lifeexpectancy/app.R
@@ -3,9 +3,16 @@
 library(shiny)
 
 ui <- fluidPage(
+  sliderInput(inputId = "years", label = "Choose a year", value = 1950, 
+              min = 1900, max = 2017)
 )
 
 server <- function(input, output) {
+  histogram <- eventReactive(input$years, {
+    hist(rnorm(input$num))
+  })
+  output$hist <- renderPlot(histogram())
+  
 }
 
 shinyApp(ui = ui, server = server)
