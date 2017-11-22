279d066aee32443d90e5655a46dfd3f29135ad8e
MJ <melaniejane.pascual@gmail.com>
2017-11-22 14:34:55 -0800

    rip

--- hw/hw3/lifeexpectancy/app.R
+++ hw/hw3/lifeexpectancy/app.R
@@ -1,14 +1,12 @@
 # Demonstrate the changes in life expectancy for each country/continent over time 
 # using a Shiny App.
+
 library(shiny)
 library(tidyverse)
 library(plyr)
 library(dplyr)
 
-## TO DO CHECKLIST:
-# FIGURE OUT WHY IT ISNT PLOTTING
-
-## create super data set, clean up everything else 
+# clean up variables 
 lifespan1 <- read.csv("lifeexpectancy.csv",check.names = F)
 income1 <- read.csv("incomepp.csv", check.names = F)
 population1 <- read.csv("population.csv", check.names = F)
@@ -26,25 +24,38 @@ lifespan <- rename(lifespan, c("Life expectancy" = "country"))
 income <-  rename(income, c("GDP per capita" = "country"))
 population <- rename(population, c("Total population" = "country"))
 
+# organize the super dataset
 draft <- join(lifespan, income)
-
-## the super dataset
 final <- join(draft, population)
-
-write.csv(final, file = "country.csv")
+# write.csv(final, file = "country.csv")
 
 country <- read.csv("country.csv")
 
-## why is income decreasing!
-test <- country %>%
-  filter(year == 2010) %>%
-  select(country, income)
+# artsy
+asia <-  
+  country %>%
+    filter(continent == 1)
+africa <- 
+  country %>%
+    filter(continent == 2)
+europe <- 
+  country %>%
+    filter(continent == 3)
 
+australia <- 
+  country %>%
+    filter(continent == 4)
+north.america <-
+  country %>%
+  filter(continent == 5)
+south.america <-
+  country %>%
+  filter(continent == 6)
 
-# actiual program 
+# Application  
 ui <- fluidPage(
   sliderInput(inputId = "year", label = "Choose a year", value = 1800, 
-              min = 1800, max = 2010, step = 10),
+              min = 1800, max = 2010, step = 10, animate = TRUE),
   plotOutput("plot1")
   
 )  
@@ -54,7 +65,12 @@ server <- function(input, output) {
                 country %>%
                   filter(year == input$year) %>%
                   ggplot(aes(x = income, y = lifespan)) +
-                  geom_point(stat="identity", aes(size = population))
+                  geom_point(aes(color = factor(continent, 
+                    labels = c("Asia","Africa","Europe", "Australia",
+                     "North America", "South America")), size = population)) +
+                  labs(color = "Continent", population = "Population") 
+            
+                              
             })
   output$plot1 <- renderPlot(
    scatter()
